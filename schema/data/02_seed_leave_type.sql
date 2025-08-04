43bc5f102cb0cdd4ec31eb1a7fa1b43d28d6573c
Bhuvnesh Singh Chauhan <47275661+bhuvneshsinghchauhan@users.noreply.github.com>
2025-08-04 12:04:11 +0530

    Added Sample Data for Leave Type table

---
+++ schema/data/02_seed_leave_type.sql
@@ -0,0 +1,8 @@
+insert into leave_type (type_name,max_days_annual) 
+values ('casual','10');
+
+insert into leave_type(type_name,max_days_annual)
+values('paid','15');
+
+insert into leave_type(type_name,max_days_annual)
+values('bereavement','5');
