bd31afc0644432537d8c7cb70bb5be9f458c0361
Bhuvnesh Singh Chauhan <47275661+bhuvneshsinghchauhan@users.noreply.github.com>
2025-08-04 12:40:44 +0530

    Added Sample Data for Holiday Table

---
+++ schema/data/04_seed_holiday.sql
@@ -0,0 +1,11 @@
+insert into holiday (HOLI_DATE,DESCRIPTION,COUNTRY_CODE)
+values (08/15/2025,'Independence Day','IN');
+
+insert into holiday (HOLI_DATE,DESCRIPTION,COUNTRY_CODE)
+values (01/26/2025,'Republic Day','IN');
+
+insert into holiday (HOLI_DATE,DESCRIPTION,COUNTRY_CODE)
+values (10/21/2025,'Diwali','IN');
+
+insert into holiday (HOLI_DATE,DESCRIPTION,COUNTRY_CODE)
+values (03/03/2026,'Holi','IN');
