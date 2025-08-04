8d1458e35946efc0f443be4b8e946b94f52cc723
Bhuvnesh Singh Chauhan <47275661+bhuvneshsinghchauhan@users.noreply.github.com>
2025-08-04 13:18:20 +0530

    Added Sample Data for App User Table

---
+++ schema/data/07_seed_app_user.sql
@@ -0,0 +1,6 @@
+-- Note: pwd_hash is placeholder (later we’ll hash it properly)
+INSERT INTO app_user (login_name, pwd_hash, emp_id)
+VALUES ('Rohit', 'plain_rohit_pw', 24);
+
+INSERT INTO app_user (login_name, pwd_hash, emp_id)
+VALUES ('Sachin', 'plain_sachin_pw', 25);
